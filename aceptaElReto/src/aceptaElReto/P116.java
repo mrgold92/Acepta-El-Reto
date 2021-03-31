78ba1639354b2522582d505e8be6dcc4c085cdd9
David Salazar <davids1_4@hotmail.com>
2021-03-31 19:17:53 +0200

    Add 116

---
+++ aceptaElReto/src/aceptaElReto/P116.java
@@ -0,0 +1,19 @@
+package aceptaElReto;
+
+import java.util.Scanner;
+
+public class P116 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int vueltas = sc.nextInt();
+		
+		for (int i = 0; i < vueltas; i++) {
+			System.out.println("Hola mundo.");
+		}
+		
+		sc.close();
+	}
+
+}
